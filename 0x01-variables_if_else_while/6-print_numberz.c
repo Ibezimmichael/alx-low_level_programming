08d4d952527e8c7789f13a77bec13914337440d6
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-12 09:24:02 +0100

    new commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *main - prints single digits od base 10
+ *followed by a new line
+ *Return: Always zero
+ */
+
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
