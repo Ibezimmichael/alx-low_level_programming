ddf9175bce8940732562643919ec8d3853e6401d
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-12 01:53:25 +0100

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - print numbers 1-10
+ *followed by a new line
+ *Return: Always 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 10; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
