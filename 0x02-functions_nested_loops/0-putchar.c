7327b8aed0b5b491a2b829977d1649a699e05b92
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-16 00:00:25 +0100

    new task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,16 @@
  *program prints _putchar
  *Return: always(0)
  */
-int _putchar(char c)
+int main(void)
 {
-char text[20] = "_putchar";
-_putchar(text);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
