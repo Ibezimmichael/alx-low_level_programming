269eedf255f6bd60a1101c1424ab2cb22b7879a4
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-16 09:30:20 +0100

    new commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet - print alphabet 10 times
+ * print_alphabet_x10 - print alphabet 10 times
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 int a;
 int i;
