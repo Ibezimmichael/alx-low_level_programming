93b6f89c53444aff75747843172af74b1932402e
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-17 07:52:19 +0100

    new commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,12 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-while (i < 10)/* the loop will continue as the conditon is always true*/
-{
-putchar(i);
-}
-
+/*
+ *while (i < 10)
+ *{
+ *putchar(i);
+ *}
+ */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
