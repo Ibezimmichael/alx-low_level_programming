69b45f1a9fc80787d2a8d4973988d1cc4a5ba839
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-18 01:13:32 +0100

    new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - prints the numbers from 1 to 100 followed by a new line
+ * for multiples of 3 print Fizz
+ * for multiples of 5 print Buzz
+ * for multipes of 3 and 5 print FizzBuzz
  *
- * Return: 0
+ * Return: returns 0
  */
 
 int main(void)
