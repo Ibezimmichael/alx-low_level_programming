fd0b0a42351f41826570fdc0e44ddbcbfb743207
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-21 19:05:35 +0100

    new commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_puts - prints a string followed by a new line
+ *@str:conatains the value
+ *
+ *Return: always 0
+ */
+void _puts(char *str)
+{
+char *m;
+int i;
+
+m = str;
+
+for (i = 0; m[i]; i++)
+{
+_putchar (m[i]);
+}
+_putchar('\n');
+}
