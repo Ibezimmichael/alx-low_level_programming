2ff897ebd1d63817da494185125b22c21e839567
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-21 17:47:40 +0100

    new commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_strlen - calculates length of a string
+ *@s: value to be calculated
+ *
+ *Return: always 0
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+}
