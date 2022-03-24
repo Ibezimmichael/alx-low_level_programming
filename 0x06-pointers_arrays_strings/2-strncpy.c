baa4b46667d622fc418e93147b348c8c3444c256
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-24 13:41:06 +0100

    new commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,14 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int j;
-for (j = 0; src[j] != '\0' && j < n; j++)
+int i;
+for (i = 0; src[i] != '\0' && i < n; i++)
 {
-dest[j] = src[j];
+dest[i] = src[i];
 }
-for (j = 0; j < n; j++)
+while (i < n)
 {
-dest[j] = '\0';
+dest[i] = '\0';
+i++;
 }
 return (dest);
 }
