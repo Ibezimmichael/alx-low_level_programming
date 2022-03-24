2bc8088e8540c71455c7e3e58f0b7a3f5c7d6e91
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-24 11:41:58 +0100

    new commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,8 @@ char *_strncat(char *dest, char *src, int n)
 int i;
 int j;
 for (i = 0; dest[i] != '\0'; i++)
-for (j = 0; j > n && src[j] != '\0'; j++, i++)
+;
+for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
 {
 dest[i] = src[j];
 }
