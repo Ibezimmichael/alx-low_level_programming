96fd87b9df60cfce92c4a6a781f3ecf9ced9ab31
Ibezimmichael <ibezim.michael@yahoo.com>
2022-03-28 13:41:03 +0100

    new commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_memset - sets a block of memmory to a value
+ *@s: pointer to be filled
+ *@b: vale to be set
+ *@n: bytes of memory
+ *Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+*(s+i) = b;
+return (s);
+}
